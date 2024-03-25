5807158d46831f81a13804c22074ae9d6fbfe8d1
Davi Fernandes <davifncosta@gmail.com>
2024-03-25 12:50:04 -0300

    feat: add task 5

--- client-app-a/src/main/java/com/example/clientappa/controller/TestController.java
+++ client-app-a/src/main/java/com/example/clientappa/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.clientappa.controller;
 
 import com.example.clientappa.client.GetRandomNumberFromB;
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Controller
 @RequestMapping("/appa")
 public class TestController {
-    
+
     @Autowired
     GetRandomNumberFromB getRandomNumberFromB;
 
     @GetMapping("/random_number")
     public ResponseEntity<Number> getNumberResponse() {
-        return getRandomNumberFromB.randomNumber();
-    }
+        try {
+            ResponseEntity<Number> responseFromB = getRandomNumberFromB.randomNumber();
 
+            if (responseFromB.getStatusCode() == HttpStatus.OK) {
+                Number numberFromB = responseFromB.getBody();
+                // Aqui você pode adicionar a lógica que deseja aplicar ao número recebido do serviço B
+                // Por exemplo, somar um número aleatório
+                return ResponseEntity.ok(numberFromB);
+            } else {
+                // Se a chamada para o endpoint falhar, retorna -2
+                return ResponseEntity.ok(-1);
+            }
+        } catch (FeignException e) {
+            // Se ocorrer uma exceção ao chamar o endpoint, retorna -2
+            return ResponseEntity.ok(-1);
+        }
+    }
 }
--- client-app-b/src/main/java/com/example/clientappb/controller/TestController.java
+++ client-app-b/src/main/java/com/example/clientappb/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.clientappb.controller;
 
 import com.example.clientappb.client.GetRandomNumberFromC;
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,18 +18,24 @@ public class TestController {
     @Autowired
     GetRandomNumberFromC getRandomNumberFromC;
 
-      @GetMapping("/random_number")
-        public ResponseEntity<Number> getNumberResponse() {
-      
-        ResponseEntity<Number> responseFromC = getRandomNumberFromC.randomNumber();
-        Number numberFromC = responseFromC.getBody();
+    @GetMapping("/random_number")
+    public ResponseEntity<Number> getNumberResponse() {
+        try {
+            ResponseEntity<Number> responseFromC = getRandomNumberFromC.randomNumber();
 
-     
-        Random random = new Random();
-        int randomNumber = random.nextInt(10) + 10;
-
-        int sum = randomNumber + numberFromC.intValue();
-
-        return ResponseEntity.ok(sum);
+            if (responseFromC.getStatusCode() == HttpStatus.OK) {
+                Number numberFromC = responseFromC.getBody();
+                Random random = new Random();
+                int randomNumber = random.nextInt(10) + 10;
+                int sum = randomNumber + numberFromC.intValue();
+                return ResponseEntity.ok(sum);
+            } else {
+                // Se a chamada para o endpoint falhar, retorna -1
+                return ResponseEntity.ok(-2);
+            }
+        } catch (FeignException e) {
+            // Se ocorrer uma exceção ao chamar o endpoint, retorna -1
+            return ResponseEntity.ok(-2);
+        }
     }
 }
